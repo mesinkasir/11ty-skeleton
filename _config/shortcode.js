893b35f5eda2b0a5e5ebe76e3fff23dca87ce929
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2025-01-27 21:13:37 +0700

    Update aria-label

--- _config/shortcode.js
+++ _config/shortcode.js
@@ -1,4 +1,4 @@
 export default function(eleventyConfig) {
 eleventyConfig.addShortcode("hello",(title,content,link,url) =>`<h3>${title}</h3><p>${content} <a href="${url}">${link}</a> `,);
-eleventyConfig.addShortcode("button",(title,url) =>`<a href="${url}" class="button no">${title}</a> `,);
+eleventyConfig.addShortcode("button",(title,url) =>`<a href="${url}" aria-label="${title}" class="button no">${title}</a> `,);
 };
